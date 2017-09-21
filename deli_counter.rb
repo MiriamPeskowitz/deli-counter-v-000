0c57b4084330da690d65ce34da6fbc64dd5e3f62
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-21 03:11:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ def line(katz_deli_line)
     if katz_deli_line.empty?
         puts "The line is currently empty."
     else
-      katz_deli_line.each_with_index.map do |person, index|
-        line_right_now << "#{index + 1}. #{person}"
+      katz_deli_line.each_with_index.map! do |person, index|
+        #line_right_now << "#{index + 1}. #{person}"
         end
-      puts "The line is currently: #{line_right_now}"
+      puts "The line is currently: #{katz_deli_line}}"
      end
 end
 
