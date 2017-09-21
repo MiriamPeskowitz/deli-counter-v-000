84e660d2c80bd57db1f08b5a9b7358e86e4f8b6a
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-21 02:54:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli_line)
         
           puts "The line is currently: #{index}. #{name}"
         end
-    end  
+    end
 end
 
 def take_a_number(katz_deli_line, wants_to_join)
