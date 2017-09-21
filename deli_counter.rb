3e795324f9bee0b00d895f02c60aed713dc3dfbf
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-21 02:54:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ def line(katz_deli_line)
     if katz_deli_line.empty?
         puts "The line is currently empty."
     else
-      line_right_now = katz_deli_line.each_with_index(1) do|name, index|
+      line_right_now = katz_deli_line.each_with_index(1).map do|name, index|
+        
           puts "The line is currently: #{index}. #{name}"
         end
     end  
@@ -23,4 +24,3 @@ def now_serving(katz_deli_line)
 
   katz_deli_line.shift
 end
-
