f351001c03b61f60948ab3d3a8e61cd4d3b7b4b6
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-21 03:24:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli_line)
     else
       line_right_now = "The line is currently: "
       katz_deli_line.each_with_index.map do |person, index|
-        line_right_now << "#{index + 1}. #{person} "
+        line_right_now << " #{index + 1}. #{person}"
       end
       puts line_right_now
     end
